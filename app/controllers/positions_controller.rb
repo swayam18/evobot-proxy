993705b63d77de120c0c9f84f9285f503434159c
Swayam <swayam.narain@gmail.com>
2014-11-04 12:20:59 +0800

    api changes

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,39 +1,25 @@
 class PositionsController < ApplicationController
   def index
     @positions = Position.all
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @positions}
-    end
+    render json: @positions
   end
 
   def predator
     @position = Position.where(name: 'predator').limit(1).first
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @position}
-    end
+    render json: @position
   end
 
   def prey
     @position = Position.where(name: 'prey').limit(1).first
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @position}
-    end
+    render json: @position
   end
 
   def create
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json do
-        @position = Position.create(position_params) 
-        if @position.save
-          render json: @position
-        else
-          render json: { errors: @position.errors.full_messages }, status: 422
-        end
-      end
+    @position = Position.create(position_params) 
+    if @position.save
+      render json: @position
+    else
+      render json: { errors: @position.errors.full_messages }, status: 422
     end
   end
 
