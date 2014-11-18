1f0429467966d905b3b76a9425eceaee5fe66698
Swayam <swayam.narain@gmail.com>
2014-11-18 22:43:01 +0800

    reluctant update of graph

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @prey = Robot.find_by_name('prey').positions.limit(30).map(&:xy)
-    @predator = Robot.find_by_name('predator').positions.limit(30).map(&:xy)
+    @prey = Robot.find_by_name('prey').positions.limit(30).map(&:xy).reverse()
+    @predator = Robot.find_by_name('predator').positions.limit(30).map(&:xy).reverse()
   end
 end
