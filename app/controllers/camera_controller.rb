52aaa68edc489f1757b968def2d6c549183b061c
Swayam <swayam.narain@gmail.com>
2014-11-26 02:03:13 +0800

    removes vagrant byebug

--- app/controllers/camera_controller.rb
+++ app/controllers/camera_controller.rb
@@ -2,7 +2,6 @@ class CameraController < ApplicationController
   def update
     camera = Camera.first
     camera.snapshot = params[:snapshot]
-    byebug
     camera.save
     render json: { success: true }
   end
