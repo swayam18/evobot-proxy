8ced625e246bf5512273faf2cee33d8dd769c16f
Swayam <swayam.narain@gmail.com>
2014-11-26 18:57:45 +0800

    removes clearing of data to make space

--- app/models/controller_error.rb
+++ app/models/controller_error.rb
@@ -1,19 +1,4 @@
 class ControllerError < ActiveRecord::Base
   belongs_to :robots
   default_scope order('created_at DESC')
-  before_create :check_count
-
-  def self.clear
-    if Rails.env == 'production'
-      ControllerError.delete_all
-      return true
-    end
-    false
-  end
-
-  def check_count
-    if ControllerError.count > 4000
-      ControllerError.clear
-    end
-  end
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,24 +1,9 @@
 class Position < ActiveRecord::Base
   belongs_to :robots
-  before_create :check_count
   default_scope order('created_at DESC')
 
   validates :x, presence: true
   validates :y, presence: true
-
-  def self.clear
-    if Rails.env == 'production'
-      Position.delete_all
-      true
-    end
-    false
-  end
-
-  def check_count
-    if Position.count > 4000
-      Position.clear
-    end
-  end
 
   def previous
     Position.where("name=? AND id < ?",name,id).first
