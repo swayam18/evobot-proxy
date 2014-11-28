f3ccd5d601526662982677cf60caf39983a56877
Swayam <swayam.narain@gmail.com>
2014-11-28 16:31:48 +0800

    fixes to s3

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,5 +1,5 @@
 class Camera < ActiveRecord::Base
-  has_attached_file :snapshot, :storage => :s3, :bucket => ENV['evobotsartsci']
+  has_attached_file :snapshot 
 
   validates_attachment_content_type :snapshot, :content_type => /\Aimage/
   validates_attachment_file_name :snapshot, :matches => [/png\Z/, /jpe?g\Z/]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,9 @@ EvobotProxy::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['evobotsartsci'],
-      :access_key_id => ENV['AKIAIAJ3DSKE6FG473AA'],
-      :secret_access_key => ENV['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']
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
