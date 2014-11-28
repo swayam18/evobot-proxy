abc400051e5e9f0bb240caf8b9f147c48c74b960
Swayam <swayam.narain@gmail.com>
2014-11-28 15:58:05 +0800

    fixes s3 integration bug

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,5 +1,6 @@
 class Camera < ActiveRecord::Base
-  has_attached_file :snapshot
+  has_attached_file :snapshot, :storage => :s3, :bucket  => ENV['MY_BUCKET_NAME']
+
   validates_attachment_content_type :snapshot, :content_type => /\Aimage/
   validates_attachment_file_name :snapshot, :matches => [/png\Z/, /jpe?g\Z/]
 end
